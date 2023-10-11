67251e0cf09fe66f325664d3e4cd220dd35560ba
Sıtkı Alagöz <sitki.alagoz@obss.tech>
2023-10-12 00:37:08 +0300

    edited db data

--- amigosservices/notification/src/main/java/com/amigoscode/notification/NotificationService.java
+++ amigosservices/notification/src/main/java/com/amigoscode/notification/NotificationService.java
@@ -3,6 +3,8 @@ package com.amigoscode.notification;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @AllArgsConstructor
 public class NotificationService {
@@ -10,7 +12,14 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
 
     public void send(NotificationRequest notificationRequest) {
-        Notification notification = Notification.builder().message(notificationRequest.message()).build();
+        Notification notification = Notification
+                .builder()
+                .sender("Amigoscode")
+                .message(notificationRequest.message())
+                .toCustomerId(notificationRequest.toCustomerId())
+                .toCustomerEmail(notificationRequest.message())
+                .sentAt(LocalDateTime.now())
+                .build();
         notificationRepository.save(notification);
     }
 }
